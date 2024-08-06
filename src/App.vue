5e36806abf031e37ee03e961eeedc6c48b783000
haeunNam <nhe0103@gmail.com>
2024-08-07 01:00:50 +0900

    vue

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,96 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  안냐세요
+<div class="black-bg" v-if="모달창열렸니==true">
+  <div class="white-bg">
+    <h4>상세페이지임</h4>
+    <p>상세페이지 내용임</p>
+    <button class="modalClose" @click="모달창열렸니=false">닫기</button>
+  </div>
+</div>
+
+  <div class="menu">
+    <a v-for="naming in menus" :key="naming">{{naming}}</a>
+  </div>
+
+  <div v-for="(a,i) in 원룸들" :key="a">
+      <img :src="원룸들[i].image" alt="">
+    <h4 @click="모달창열렸니=true">{{원룸들[i].title}}</h4>
+    <p >{{원룸들[i].price}}</p>
+    <P>{{원룸들[i].content}}</P>
+    <button @click="신고수[i]++">허위매물 신고</button> 
+    <span>신고수 : {{신고수[i]}}</span>
+    
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import oneroom from "./assets/data.js";
 
 export default {
   name: 'App',
+  data(){
+    return{
+      모달창열렸니 : false,
+      신고수 : [0,0,0],
+      menus : ['Home','Shop','About'],
+      products : ['역삼동원룸','천호동원룸','마포구원룸'],
+      prices : ['50,000','10,000','8,000'],
+      원룸들 : oneroom
+    }
+  },
+  methods: {
+    increase(){
+      this.신고수++
+    },
+  },
   components: {
-    HelloWorld
+   
   }
 }
 </script>
 
 <style>
+body{
+  margin: 0;
+}
+
+div{
+  box-sizing: border-box;
+}
+
+.black-bg{
+  width: 100%;height: 100%;
+  background: rbga(0,0,0,0.5);
+  position: fixed;padding:20px;
+}
+.white-bg{
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding:20px;
+}
+.modalClose{
+  background: blueviolet;
+  color: white;
+}
 #app {
+
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
+}
+
+.menu{
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a{
+  color: white;
+  padding: 10px;
 }
 </style>
