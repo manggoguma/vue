1de9f057edd78d00ba4a2092fe03af47c16ca000
haeunNam <nhe0103@gmail.com>
2024-08-07 12:47:44 +0900

    ss

--- src/App.vue
+++ src/App.vue
@@ -1,95 +1,98 @@
 <template>
-<div class="black-bg" v-if="모달창열렸니==true">
-  <div class="white-bg">
-    <h4>상세페이지임</h4>
-    <p>상세페이지 내용임</p>
-    <button class="modalClose" @click="모달창열렸니=false">닫기</button>
+  <!-- <div class="black-bg" v-if="모달창열렸니 === true">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+      <button class="modalClose" @click="모달창열렸니=false">닫기</button>
+    </div>
   </div>
-</div>
 
   <div class="menu">
     <a v-for="naming in menus" :key="naming">{{naming}}</a>
   </div>
 
   <div v-for="(a,i) in 원룸들" :key="a">
-      <img :src="원룸들[i].image" alt="">
+    <img :src="원룸들[i].image" alt="">
     <h4 @click="모달창열렸니=true">{{원룸들[i].title}}</h4>
-    <p >{{원룸들[i].price}}</p>
-    <P>{{원룸들[i].content}}</P>
+    <p>{{원룸들[i].price}}</p>
+    <p>{{원룸들[i].content}}</p>
     <button @click="신고수[i]++">허위매물 신고</button> 
     <span>신고수 : {{신고수[i]}}</span>
-    
-  </div>
 
+  </div> -->
+  
+    <ShipChajang /> <!-- 컴포넌트 사용 -->
 </template>
 
 <script>
 import oneroom from "./assets/data.js";
+import ShipChajang from "./shipchajang.vue";
 
 export default {
   name: 'App',
-  data(){
-    return{
-      모달창열렸니 : false,
-      신고수 : [0,0,0],
-      menus : ['Home','Shop','About'],
-      products : ['역삼동원룸','천호동원룸','마포구원룸'],
-      prices : ['50,000','10,000','8,000'],
-      원룸들 : oneroom
+  data() {
+    return {
+      모달창열렸니: false,
+      신고수: [0, 0, 0],
+      menus: ['Home', 'Shop', 'About'],
+      원룸들: oneroom,
     }
   },
   methods: {
-    increase(){
+    increase() {
       this.신고수++
     },
   },
   components: {
-   
+    ShipChajang // 컴포넌트 등록
   }
 }
 </script>
 
 <style>
-body{
+body {
   margin: 0;
 }
 
-div{
+div {
   box-sizing: border-box;
 }
 
-.black-bg{
-  width: 100%;height: 100%;
-  background: rbga(0,0,0,0.5);
-  position: fixed;padding:20px;
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0,0,0,0.5); /* rbga -> rgba */
+  position: fixed;
+  padding: 20px;
 }
-.white-bg{
+
+.white-bg {
   width: 100%;
   background: white;
   border-radius: 8px;
-  padding:20px;
+  padding: 20px;
 }
-.modalClose{
+
+.modalClose {
   background: blueviolet;
   color: white;
 }
-#app {
 
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-
 }
 
-.menu{
+.menu {
   background: darkslateblue;
   padding: 15px;
   border-radius: 5px;
 }
 
-.menu a{
+.menu a {
   color: white;
   padding: 10px;
 }
--- src/main.js
+++ src/main.js
@@ -2,3 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+
+// import Vue from 'vue'
+// // App.vue 대신 다른 컴포넌트를 불러옵니다
+// import shipchajang from './shipchajang.vue'
+
+// Vue.config.productionTip = false
+
+// new Vue({
+//   render: h => h(shipchajang), // App.vue가 아닌 OtherComponent.vue를 렌더링
+// }).$mount('#app')
+
